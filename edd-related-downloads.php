300a78d322fa847c15e6ca00f03fc02f7a625f4e
isabel104 <isabel104@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-05 23:08:52 +0000

    test 2 changes
    git-svn-id: http://plugins.svn.wordpress.org/easy-digital-downloads-related-downloads/trunk@692511 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ edd-related-downloads.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Plugin Name: Easy Digital Downloads - Related Downloads
+Plugin URI: http://isabelcastillo.com/edd-related-downloads-plugin/
+Description: Show related downloads by tag or category when using Easy Digital Downloads plugin.
+Version: 1.0
+Author: Isabel Castillo
+Author URI: http://isabelcastillo.com
+License: GPL2
+Text Domain: edd-related-downloads
+Domain Path: languages
+
+Copyright 2013 Isabel Castillo
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+if(!class_exists('EDD_Related_Downloads')) {
+class EDD_Related_Downloads{
+    public function __construct() {
+
+		add_action( 'edd_after_download_content', array( $this, 'isa_after_download_content' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+	    add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+		add_filter( 'edd_settings_misc', array( $this, 'isa_eddrd_add_settings' ) );
+
+
+    }
+
+   	public function enqueue() {
+			
+		if ( is_singular( 'download' ) ) {
+	            wp_register_style('edd-related-downloads', plugins_url('/edd-related-downloads.css', __FILE__));
+	            wp_enqueue_style('edd-related-downloads');
+		}
+	}
+
+
+	public function load_textdomain() {
+
+		load_plugin_textdomain( 'edd-related-downloads', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+	}
+
+	/**
+	 * Add settings to the "Easy Digital Downloads > Settings > Misc" section
+	 *
+	 * @since 1.0
+	 */
+	function isa_eddrd_add_settings( $settings ) {
+	
+		$isa_eddrd_settings = array(
+			array(
+				'id' => 'isa_eddrd_settings',
+				'name' => '<strong>'. __('Related Downloads Settings', 'edd-related-downloads') . '</strong>',
+				'desc' => __( 'Settings for EDD Related Downloads Plugin.', 'edd-related-downloads'),
+				'type' => 'header'
+			),
+			array(
+				'id' => 'related_filter_by_cat',
+				'name' => __('Filter related downloads by category:', 'edd-related-downloads'), 
+				'desc' => __( 'Check this to filter by category. By default, downloads are related by tag.', 'edd-related-downloads'),
+				'type' => 'checkbox'
+			),
+			array(
+				'id' => 'related_showposts_num',
+				'name' => __('How many related items to show:', 'edd-related-downloads'), 
+				'desc' => __( 'Enter a decent number, like between 1 and 7. Default is 3.', 'edd-related-downloads'),
+				'type' => 'text',
+			),
+			array(
+				'id' => 'related_dl_title',
+				'name' => __('Custom Related Downloads Title:', 'edd-related-downloads'), 
+				'desc' => __( 'This appears above the related items. Default is, "You May Also Like".', 'edd-related-downloads'),
+				'type' => 'text'
+			),
+
+		);
+	
+		/* Merge plugin settings with original EDD settings */
+		return array_merge( $settings, $isa_eddrd_settings );
+
+	}
+
+	/**
+	 * Adds related items on the single download page, underneath content
+	 *
+	 * @since 0.1
+	 */
+
+	public 	function isa_after_download_content() {
+	    global $post, $data, $edd_options;
+		$taxchoice = isset( $edd_options['related_filter_by_cat'] ) ? 'download_category' : 'download_tag';
+		$custom_taxterms = wp_get_object_terms( $post->ID, $taxchoice, array('fields' => 'ids') );
+
+		$howmany = ( 
+						isset( $edd_options['related_showposts_num'] ) && 
+						! empty( $edd_options['related_showposts_num'] )
+					)
+					? $edd_options['related_showposts_num'] : 3;// 1.1
+
+		$related_dl_title = ( 
+								isset( $edd_options['related_dl_title'] ) && 
+								( $edd_options['related_dl_title'] != '' )
+							)
+							? $edd_options['related_dl_title'] : __('You May Also Like', 'edd-related-downloads');// 1.1
+	    $args = array(
+				'post_type' => 'download',
+	            'post__not_in' => array($post->ID),
+	            'showposts' => $howmany,// 1.1
+				'tax_query' => array(
+						array(
+							'taxonomy' => $taxchoice,
+							'field' => 'id',
+							'terms' => $custom_taxterms
+						)
+					)
+	    );
+	 
+	    $eddrd_query = new WP_Query($args);
+	        if( $eddrd_query->have_posts() ) {
+				?>
+				<div id="isa-related-downloads"><h3>
+	            <?php echo $related_dl_title; ?>
+				</h3><ul>
+				<?php 
+	            while ($eddrd_query->have_posts()) {
+	                $eddrd_query->the_post();
+
+					if(has_post_thumbnail()) {
+						$thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumbnail' );
+					}
+		            ?>
+	                <li>
+						<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
+							<img class="wp-post-image" alt="<?php the_title_attribute(); ?>" src="<?php echo $thumb[0]; ?>" />
+							<?php the_title(); ?>
+						</a>
+					</li>
+          <?php } ?>
+          </ul></div>
+		<?php wp_reset_query();
+			}
+	}
+}
+}
+$EDD_Related_Downloads = new EDD_Related_Downloads();
---
+++ readme.txt
@@ -0,0 +1,57 @@
+﻿=== Easy Digital Downloads - Related Downloads ===
+Author URI: http://isabelcastillo.com
+Plugin URI: http://isabelcastillo.com/edd-related-downloads-plugin/
+Contributors: isabel104
+Tags: related posts, custom, taxonomy, taxoxnomies, download category, download tag, downloads categories, downloads tags, easy digital downloads, related items, related products
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable Tag: 1.0
+License: GNU Version 2 or Any Later Version
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Show related downloads by tag or category when using Easy Digital Downloads plugin.
+
+== Description ==
+
+This is an extension for [Easy Digital Downloads](http://wordpress.org/extend/plugins/easy-digital-downloads/) that automatically adds related downloads after the single download content on the single download page. 
+It is simple and light. It works by default without a need for any settings. By default, 3 related downloads will be shown, related by download_tag. The title and featured image will be displayed, centered nicely. 
+
+**Unobtrusive Styling - Fits right into your theme**
+
+*   The featured images adopt your theme's style for `.wp-post-image`.
+*	You can modify the image style with this selector: `#isa-related-downloads img `
+*	Responsive CSS is included so it looks good on any size screen.
+
+**Works automatically. No settings needed.**
+
+This plugin has only 3 settings, and they are optional. These 3 options are located at `Downloads --> Settings --> Misc tab`.
+
+**Setting 1: Filter by Category or Tag**
+
+By default, downloads are related by category. You can checkmark this option to choose to filter by tag instead.
+
+**Setting 2: Change the number of related items to show**
+
+By default, 3 related items are shown. You have the option to change this number.
+
+**Setting 3: Custom Related Downloads Title:**
+
+By default, the related items are headed by, "You May Also Like". You have the option to enter a custom title.
+
+For more info, go to [EDD Related Downloads](http://isabelcastillo.com/edd-related-downloads-plugin/) 
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Why aren't there any related downloads showing up on my single download page? =
+Most likely, there are no other downloads that share that download's tag or category.
+
+== Changelog ==
+
+= 1.0: April 5, 2013 =
+
+* Initial release.
